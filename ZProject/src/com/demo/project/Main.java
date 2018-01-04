71811991dfaae024c753d5645b3244994f19b617
Raghu <Raghu@Raghu-PC>
2018-01-04 11:31:08 +0530

    Initial Commit

---
+++ ZProject/src/com/demo/project/Main.java
@@ -0,0 +1,16 @@
+package com.demo.project;
+
+public class Main {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+	Main1 main=new Main1();	
+	 main.print();	
+	display();
+
+	}
+ public static void display() 
+ {
+	 System.out.println("This is first git repository");
+ }
+}
---
+++ ZProject/src/com/demo/project/Main1.java
@@ -0,0 +1,9 @@
+package com.demo.project;
+
+public class Main1 {
+	
+  public static void print() 
+  {
+	  System.out.println("Print this statement");
+  }
+}
